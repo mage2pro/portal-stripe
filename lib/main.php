235a47658a37cfe9f434904ce8e3cd9755d4aed0
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-07 01:02:04 +0000

    refactoring

--- lib/main.php
+++ lib/main.php
@@ -4,7 +4,7 @@
  * The result's format: https://github.com/mage2pro/portal-stripe/blob/0.4.1/etc/customers.json
  * @return array(array(string => string))
  */
-function dfe_portal_stripe_customers() {return df_cache_get_simple(__FUNCTION__, function() {
+function dfe_portal_stripe_customers():array {return df_cache_get_simple(__FUNCTION__, function() {
 	$customers = df_map_r(function(array $a) {return [
 		$a['id'], dfa_deep($a, 'relationships/country/data/id')
 	];}, df_oro_get_list('customers')['data']); /** @var array(string => string) $customers */
@@ -16,7 +16,7 @@ function dfe_portal_stripe_customers() {return df_cache_get_simple(__FUNCTION__,
 					'extenddfwebsites' === $a['type']
 					&& 'magento_2' === dfa_deep($a, 'relationships/platform/data/id')
 				;}
-			), null, function(array $a) {return dfa_deep($a, 'attributes/domain');}
+			), '', function(array $a) {return dfa_deep($a, 'attributes/domain');}
 		), function(array $a) use($customers) {$at = $a['attributes']; return [
 			# 2017-07-13 From now on, country can be defined not only for a customer, but for a website too.
 			'country' => dfa_deep($a, 'relationships/country/data/id',
